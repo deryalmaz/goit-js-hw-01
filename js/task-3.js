0c37911329b4327e4653cef599e4e005e1a7378b
Derya Almaz <deryaalmazz@icloud.com>
2024-11-06 21:46:48 +0300

    done

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function getElementWidth(content, padding, border){
+    const cont = Number.parseFloat(content)
+    const pad = Number.parseFloat(padding)
+    const bord = Number.parseFloat(border)
+    const totalWidth = bord + pad + cont + pad + bord
+     return totalWidth
+}
+
+
+
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
